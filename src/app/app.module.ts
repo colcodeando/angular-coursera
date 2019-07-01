0fefab0e70eed8a29b841e97d972f7fcd5a0258b
Jorge Marzola <colcodeando@gmail.com>
2019-07-01 22:48:23 +0200

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -52,7 +57,11 @@ import { LoginComponent } from './login/login.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [LoginComponent],
